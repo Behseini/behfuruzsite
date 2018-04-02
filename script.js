8a0326e25e27611a0a782174acafa87656ca7c0f
Behrouz Hosseini <Behseini@users.noreply.github.com>
2018-04-02 11:37:51 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+
+
+
+  var newBanner = $("<div />", {
+ "class" : "header-new"
+});
+$( "#banner" ).before( newBanner );
+$('.header-new').html('<div class="row"><div class="container" id="boxer" style="background-color:#8ca525;"><div class="pull-left" id="evergreen"><img src="http://www.geoca.com/behfuruz/everGreen.png" alt="Vancouver Home For Buy sell"></div></div></div>');
+$('#boxer').append($('#syndication'));
+$('#syndication').addClass('pull-right');
+ var newMenu = $("<div />", {
+ "class" : "menu-new"
+});
+$('.header-new').after(newMenu);
+$('.menu-new').append($('#nav'));
+
+$( ".header-new" ).after('<div class="row" id ="bannerBox-new"><div class="container" id="newBanner"></div></div>');
+$( "#bannerBox-new > .container" ).css('background','none')
+
+$('#newBanner').append($("#banner"));
+ var newDivFooter = $("<div />", {
+ "class" : "footer-new"
+});
+$("#footer").after(newDivFooter);
+$('.footer-new').append($('#footer'));
+
+//
+
+  $('img[alt="award logos"]').wrap("<a href='http://www.behfuruz.com/award' </a>");
+
+});
